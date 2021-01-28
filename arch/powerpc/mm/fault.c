d69f79eda084d0a62fdd8fb34054ccaf68a96c40
Christophe Leroy <christophe.leroy@csgroup.eu>
2021-01-28 09:30:23 +0000

    powerpc/32: Remove handle_page_fault()
    Now that non volatile registers are saved at all time, no
    need to split bad_page_fault() out of do_page_fault().
    
    Remove handle_page_fault() and use do_page_fault() directly.

--- arch/powerpc/mm/fault.c
+++ arch/powerpc/mm/fault.c
@@ -552,11 +552,11 @@ static long __do_page_fault(struct pt_regs *regs)
 	if (likely(entry)) {
 		instruction_pointer_set(regs, extable_fixup(entry));
 		return 0;
-	} else if (IS_ENABLED(CONFIG_PPC_BOOK3S_64)) {
+	} else if (!IS_ENABLED(CONFIG_PPC_BOOK3E_64)) {
 		__bad_page_fault(regs, err);
 		return 0;
 	} else {
-		/* 32 and 64e handle the bad page fault in asm */
+		/* 64e handle the bad page fault in asm */
 		return err;
 	}
 }
