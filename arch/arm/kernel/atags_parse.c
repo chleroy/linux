6173ec9416d7106bdb251afb9f32c1df2da11a55
Christophe Leroy <christophe.leroy@csgroup.eu>
2021-03-26 10:44:59 +0000

    arm: Convert to GENERIC_CMDLINE
    This converts the architecture to GENERIC_CMDLINE.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    ---
    v4:
    - Removed dependency to ATAGS
    - Removed the message when forcing

--- arch/arm/kernel/atags_parse.c
+++ arch/arm/kernel/atags_parse.c
@@ -14,6 +14,7 @@
  * is not parsed in any way).
  */
 
+#include <linux/cmdline.h>
 #include <linux/init.h>
 #include <linux/initrd.h>
 #include <linux/kernel.h>
@@ -120,16 +121,8 @@ __tagtable(ATAG_REVISION, parse_tag_revision);
 
 static int __init parse_tag_cmdline(const struct tag *tag)
 {
-#if defined(CONFIG_CMDLINE_EXTEND)
-	strlcat(default_command_line, " ", COMMAND_LINE_SIZE);
-	strlcat(default_command_line, tag->u.cmdline.cmdline,
-		COMMAND_LINE_SIZE);
-#elif defined(CONFIG_CMDLINE_FORCE)
-	pr_warn("Ignoring tag cmdline (using the default kernel command line)\n");
-#else
-	strlcpy(default_command_line, tag->u.cmdline.cmdline,
-		COMMAND_LINE_SIZE);
-#endif
+	cmdline_build(default_command_line, tag->u.cmdline.cmdline);
+
 	return 0;
 }
 
