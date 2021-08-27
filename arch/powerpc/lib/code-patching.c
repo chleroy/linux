103c343227c2f282fc1c990d2eebcfa4eac46e83
Christophe Leroy <christophe.leroy@csgroup.eu>
2021-08-27 15:52:54 +0000

    powerpc/code-patching: Return error on patch_branch() out-of-range failure
    Do not silentely ignore a failure of create_branch() in
    patch_branch(). Return -ERANGE.

--- arch/powerpc/lib/code-patching.c
+++ arch/powerpc/lib/code-patching.c
@@ -202,7 +202,9 @@ int patch_branch(u32 *addr, unsigned long target, int flags)
 {
 	ppc_inst_t instr;
 
-	create_branch(&instr, addr, target, flags);
+	if (create_branch(&instr, addr, target, flags))
+		return -ERANGE;
+
 	return patch_instruction(addr, instr);
 }
 
